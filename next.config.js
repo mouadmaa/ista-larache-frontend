f775df0bcb8c40bb18060646800efbbb3ff268c0
mouadmaa <maaroufimouad00@gmail.com>
2020-10-19 02:55:47 +0100

    remove qpa

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 const withPlugins = require('next-compose-plugins')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
-
-const prod = process.env.NODE_ENV === 'production'
 
 const nextConfig = {}
 
 module.exports = withPlugins([
-
-  [withPWA, {
-    pwa: {
-      disable: !prod,
-      dest: 'public',
-      runtimeCaching,
-    },
-  }],
 
   [withBundleAnalyzer, {
     enabled: process.env.ANALYZE === 'true',
